a1e3d0fb405480e93f59229036086c8df450c764
nihosprop <niwibonihs@tutamail.com>
2026-01-25 17:30:33 +0300

    feat(config): update configuration models
    Define Pydantic models for bot, logs, Redis, PostgreSQL, and Stepik configurations.
    Add Pydantic field validation for log level and Redis password.
    Implement the `get_config` function to load settings from Dynaconf into these structured Pydantic models.
    Introduce an environment switcher for Dynaconf (`ENV_FOR_DYNACONF`).
    Provide robust fallback logic for database and Redis hostnames, and Redis password.
    Rename `bot_token` to `token` in `BotConfig` for improved consistency.

--- src/core/config.py
+++ src/core/config.py
@@ -1,27 +1,34 @@
 from aiogram.enums import ParseMode
 from dynaconf import Dynaconf
-from pydantic import BaseModel, SecretStr
+from pydantic import BaseModel, Field, SecretStr
 
 
 class BotConfig(BaseModel):
-    bot_token: SecretStr
+    token: SecretStr
     parse_mode: ParseMode
 
 
 class LogsConfig(BaseModel):
-    log_level: str
-
-
-class RedisConfig(BaseModel):
-    pass
-
-
-class PostgresConfig(BaseModel):
-    pass
+    log_level: str = Field(pattern="^(DEBUG|INFO)$")
 
 
 class StepikConfig(BaseModel):
-    pass
+    stepik_client_id: SecretStr
+    stepik_client_secret: SecretStr
+
+
+class RedisConfig(BaseModel):
+    host: str
+    port: int
+    password: SecretStr | None = Field(default=None, min_length=7)
+
+
+class PostgresConfig(BaseModel):
+    name: str
+    host: str
+    port: int
+    user: str
+    password: SecretStr
 
 
 class Config(BaseModel):
@@ -29,15 +36,48 @@ class Config(BaseModel):
     logs: LogsConfig
     redis: RedisConfig
     postgres: PostgresConfig
+    stepik: StepikConfig
 
 
 settings = Dynaconf(
     envvar_prefix=False,
     load_dotenv=True,
-    settings_file='settings.toml',
+    settings_files=['settings.toml'],
     environments=True,
+    env_switcher='ENV_FOR_DYNACONF',
 )
 
 
 def get_config() -> Config:
-    pass
+    bot = BotConfig(
+        token=settings.bot_token, parse_mode=settings.bot.parse_mode
+    )
+
+    logs = LogsConfig(log_level=settings.logs.log_level)
+
+    redis = RedisConfig(
+        host=settings.get(
+            'redis_host', settings.get('redis.host', 'localhost')
+        ),
+        port=settings.redis.port,
+        password=settings.get('redis_password'),
+    )
+
+    postgres = PostgresConfig(
+        name=settings.postgres.name,
+        host=settings.get(
+            'postgres_host', settings.get('postgres.host', 'localhost')
+        ),
+        port=settings.postgres.port,
+        user=settings.postgres_user,
+        password=settings.postgres_password,
+    )
+
+    stepik = StepikConfig(
+        stepik_client_id=settings.stepik_client_id,
+        stepik_client_secret=settings.stepik_client_secret,
+    )
+
+    return Config(
+        bot=bot, logs=logs, redis=redis, postgres=postgres, stepik=stepik
+    )

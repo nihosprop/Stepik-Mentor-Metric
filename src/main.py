8279dbbb7dbd7dde1772bf44e6aadc0b87b3c735
nihosprop <niwibonihs@tutamail.com>
2026-01-30 17:58:10 +0300

    refactor

--- src/main.py
+++ src/main.py
@@ -32,6 +32,7 @@ async def main() -> None:
 
     dp.include_routers(start_router, start_dialog)
     logger.info('Include routers complete')
+    
     setup_dialogs(dp)
     logger.info('Dialogs setup complete')
 

a488918046ecbd239963ee8e1822c3d7ea8e09f1
nihosprop <niwibonihs@tutamail.com>
2026-02-03 15:36:26 +0300

    feat: initialize database tables on startup
    Ensure the database schema is created or updated when the application starts.
    Integrate `create_tables` call using the `AsyncEngine` from Dishka.
    Improve application startup by deleting pending webhook updates.
    Register Dishka container closure with dispatcher shutdown for graceful termination.
    Add type hints for `AsyncContainer` and `AsyncEngine` for better code clarity.
    Update logging messages for dispatcher instance creation and shutdown process.

--- src/main.py
+++ src/main.py
@@ -3,14 +3,16 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram_dialog import setup_dialogs
-from dishka import make_async_container
+from dishka import AsyncContainer, make_async_container
 from dishka.integrations.aiogram import setup_dishka
+from sqlalchemy.ext.asyncio import AsyncEngine
 
 from bot.dialogs.start.dialog import start_dialog
 from bot.dialogs.start.handlers import start_router
 from bot.providers import ConfigProvider, DBProvider
 from core.logger import setup_logging
 from core.main_config import main_config
+from db.create_tables import create_tables
 
 logger = logging.getLogger(__name__)
 
@@ -22,27 +24,38 @@ async def main() -> None:
     bot = Bot(token=main_config.bot.token)
     logger.info('Bot instance created')
     dp = Dispatcher()
-    logger.info('DP instance created')
+    logger.info('Dispatcher instance created')
 
-    #Dishka
-    container = make_async_container(ConfigProvider(), DBProvider())
-    setup_dishka(container=container, router=dp)
+    container: AsyncContainer = make_async_container(
+        ConfigProvider(), DBProvider()
+    )
+
+    engine: AsyncEngine = await container.get(AsyncEngine)
+
+    await create_tables(engine)
+    logger.info('Database tables synced')
+
+    setup_dishka(container=container, router=dp, auto_inject=True)
     logger.info('Dishka setup complete')
-    
+
     # Routers
     dp.include_routers(start_router, start_dialog)
     logger.info('Include routers complete')
 
-    #Dialogs
+    # Dialogs
     setup_dialogs(dp)
     logger.info('Dialogs setup complete')
+
+    await bot.delete_webhook(drop_pending_updates=True)
 
     try:
         logger.info('Starting loop')
         await dp.start_polling(bot)
     finally:
-        await container.close()
-        logger.info('Shutting down')
+        dp.shutdown.register(container.close)
+        # await container.close()
+        logger.info('Dishka container closed')
+        logger.info('Closing loop')
 
 
 if __name__ == '__main__':

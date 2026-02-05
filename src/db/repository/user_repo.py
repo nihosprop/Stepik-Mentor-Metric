611a8f75ff3a989812b843b24513b1bcba765eb5
nihosprop <niwibonihs@tutamail.com>
2026-02-05 23:30:48 +0300

    feat(user-repository): implement user upsert, get, and delete operations
    Replace `add_if_not_exists` with `upsert_user` for efficient user creation and updates.
    Utilize `ON CONFLICT DO UPDATE` to atomically insert or update user records based on `telegram_id`.
    Add `get_user` method to retrieve a user by their Telegram ID.
    Introduce `delete_user` method for removing user records.
    These changes enhance the `UserRepo` with comprehensive user management capabilities.

--- src/db/repository/user_repo.py
+++ src/db/repository/user_repo.py
@@ -3,7 +3,8 @@ import logging
 from dataclasses import dataclass
 
 from aiogram.types import User as TelegramUser
-from sqlalchemy import exists, select
+from sqlalchemy import delete, exists, select
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models.user import User
@@ -18,32 +19,42 @@ class UserRepository:
     """
     session: AsyncSession
 
-    async def _user_exists(self, telegram_id: int) -> bool | None:
-        stmt = select(exists().where(User.telegram_id == telegram_id))
-        return await self.session.scalar(stmt)
-
-    async def add_if_not_exists(self, telegram_user: TelegramUser) -> None:
+    async def upsert_user(self, telegram_user: TelegramUser) -> None:
         """
-        Add a user if it doesn't exist
+        Upsert user
         Args:
-            telegram_user: Telegram user object
+            telegram_user:
+
         Returns:
             None
         """
-        telegram_id: int = telegram_user.id
-
-        if await self._user_exists(telegram_id):
-            logger.debug(f'User {telegram_id} already exists')
-            return
-
-        user = User(
-            telegram_id=telegram_id,
+        insert_stmt = insert(User).values(
+            telegram_id=telegram_user.id,
             first_name=telegram_user.first_name,
             last_name=telegram_user.last_name,
             username=telegram_user.username,
         )
-        self.session.add(user)
-
-        logger.info(
-            f'Created new user {telegram_id}:{telegram_user.first_name}'
+        upsert_stmt = insert_stmt.on_conflict_do_update(
+            index_elements=['telegram_id'],
+            set_={
+                'first_name': telegram_user.first_name,
+                'last_name': telegram_user.last_name,
+                'username': telegram_user.username,
+            },
         )
+        await self.session.execute(upsert_stmt)
+        logger.debug(f'Upserted user {telegram_user.id}')
+
+    async def _user_exists(self, telegram_id: int) -> bool | None:
+        stmt = select(exists().where(User.telegram_id == telegram_id))
+        return await self.session.scalar(stmt)
+
+    async def get_user(self, telegram_user: TelegramUser) -> User | None:
+        if await self._user_exists(telegram_user.id):
+            stmt = select(User).where(User.telegram_id == telegram_user.id)
+            return await self.session.scalar(stmt)
+        return None
+
+    async def delete_user(self, telegram_user: TelegramUser) -> None:
+        stmt = delete(User).where(User.telegram_id == telegram_user.id)
+        await self.session.execute(stmt)

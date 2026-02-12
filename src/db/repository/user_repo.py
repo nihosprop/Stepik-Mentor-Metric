9490b3f30b00aeca4e7a4729cf327f5c49f70513
nihosprop <niwibonihs@tutamail.com>
2026-02-12 17:48:17 +0300

    refactor

--- src/db/repository/user_repo.py
+++ src/db/repository/user_repo.py
@@ -7,7 +7,7 @@ from sqlalchemy import delete, exists, select
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.db.models.user import User
+from db.models.user import User
 
 logger = logging.getLogger(__name__)
 

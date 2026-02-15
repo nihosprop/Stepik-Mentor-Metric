9d4df591b6d3213447a3b72837bbdd41947f426c
nihosprop <niwibonihs@tutamail.com>
2026-02-15 23:36:21 +0300

    refactor: rename method to improve clarity of config retrieval

--- src/infrastructure/di/providers/config.py
+++ src/infrastructure/di/providers/config.py
@@ -5,5 +5,5 @@ from core.main_config import Config, main_config
 
 class ConfigProvider(Provider):
     @provide(scope=Scope.APP)
-    def get_config(self) -> Config:
+    def config(self) -> Config:
         return main_config

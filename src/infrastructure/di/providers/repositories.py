e79587f5a6155efc1ffb6f6e87edc9be13fabc2d
nihosprop <niwibonihs@tutamail.com>
2026-02-15 23:15:32 +0300

    refactor: rename method to clarify user repository retrieval

--- src/infrastructure/di/providers/repositories.py
+++ src/infrastructure/di/providers/repositories.py
@@ -11,7 +11,7 @@ class RepositoryProvider(Provider):
     Repository Provider
     """
     @provide(scope=Scope.REQUEST)
-    def get_user_repo(self, session: AsyncSession) -> UserRepository:
+    def get_tg_user_repo(self, session: AsyncSession) -> UserRepository:
         """
         Get User Repository
         Args:

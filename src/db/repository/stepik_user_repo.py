c5125f6537a4614a9ea093230804fbc8d4551827
nihosprop <niwibonihs@tutamail.com>
2026-02-16 00:29:58 +0300

    feat: add stepik user repository
    Create a new `StepikUserRepo` class to handle database operations for Stepik users.
    Implement `upsert_user` to efficiently insert or update user details based on `user_id`.
    Provide `get_stepik_user` to retrieve Stepik user objects by their ID.

---
+++ src/db/repository/stepik_user_repo.py
@@ -0,0 +1,45 @@
+import logging
+
+from dataclasses import dataclass
+
+from sqlalchemy import exists, select
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models.stepik_user import StepikUser
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class StepikUserRepo:
+    session: AsyncSession
+
+    async def _user_exists(self, stepik_user_id: int) -> bool | None:
+        stmt = select(exists().where(StepikUser.user_id == stepik_user_id))
+        return await self.session.scalar(stmt)
+
+    async def upsert_user(
+        self, stepik_user_id: int, full_name: str, is_mentor: bool = False
+    ) -> None:
+        insert_stmt = insert(StepikUser).values(
+            user_id=stepik_user_id,
+            full_name=full_name,
+            is_mentor=is_mentor
+        )
+        upsert_stmt = insert_stmt.on_conflict_do_update(
+            index_elements=['user_id'],
+            set_={
+                'full_name': full_name,
+                'is_mentor': is_mentor,
+            },
+        )
+        await self.session.execute(upsert_stmt)
+        logger.debug(f'Upserted Stepik user {stepik_user_id}')
+
+    async def get_stepik_user(self, stepik_user_id: int) -> StepikUser | None:
+        """
+        Gets a user object by ID.
+        """
+        stmt = select(StepikUser).where(StepikUser.user_id == stepik_user_id)
+        return await self.session.scalar(stmt)

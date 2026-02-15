88beee2164dee8ffdd0679fc4657aecb5477a5a4
nihosprop <niwibonihs@tutamail.com>
2026-02-16 00:34:44 +0300

    feat(providers): add stepik user repository provider
    Introduce a new provider for the Stepik user repository.
    This enables dependency injection of the `StepikUserRepo` instance across the application.
    Add the required import for `StepikUserRepo`.
    Update the docstring for the `tg_user_repo` provider for improved clarity.r

--- src/infrastructure/di/providers/repositories.py
+++ src/infrastructure/di/providers/repositories.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import (
     AsyncSession,
 )
 
+from db.repository.stepik_user_repo import StepikUserRepo
 from db.repository.user_repo import UserRepository
 
 
@@ -10,13 +11,25 @@ class RepositoryProvider(Provider):
     """
     Repository Provider
     """
+
     @provide(scope=Scope.REQUEST)
     def tg_user_repo(self, session: AsyncSession) -> UserRepository:
         """
-        Get User Repository
+        Telegram user repository
         Args:
             session: AsyncSession
         Returns:
             UserRepository
         """
         return UserRepository(session=session)
+
+    @provide(scope=Scope.REQUEST)
+    def stepik_user_repo(self, session: AsyncSession) -> StepikUserRepo:
+        """
+        Stepik user repository
+        Args:
+            session: AsyncSession
+        Returns:
+            StepikUserRepo
+        """
+        return StepikUserRepo(session=session)

36f8accaf1f76f0d4fac043bff38e8c5ed88aff8
nihosprop <niwibonihs@tutamail.com>
2026-02-21 16:48:38 +0300

    feat: validate mentor link and store user data
    Implement a check to ensure a valid Stepik username is retrieved for the provided mentor ID before advancing the dialog.
    Store the validated mentor ID and username in dialog_manager.dialog_data for subsequent use.
    Remove unused ShowMode import for cleaner code.
    Mark unused handler parameters with an underscore prefix for clarity.

--- src/bot/dialogs/flows/mentors/handlers.py
+++ src/bot/dialogs/flows/mentors/handlers.py
@@ -2,7 +2,7 @@ import logging
 
 from aiogram import Router
 from aiogram.types import Message
-from aiogram_dialog import DialogManager, ShowMode
+from aiogram_dialog import DialogManager
 from aiogram_dialog.widgets.input import ManagedTextInput
 from dishka.integrations.aiogram_dialog import FromDishka, inject
 
@@ -15,32 +15,37 @@ logger = logging.getLogger(__name__)
 
 @inject
 async def correct_link_to_mentor(
-    msg: Message,
-    widget: ManagedTextInput,
+    _msg: Message,
+    _widget: ManagedTextInput,
     dialog_manager: DialogManager,
     text: str,
     stepik_client: FromDishka[StepikAPIClient],
 ) -> None:
     logger.debug('Entry')
 
-    stepik_user_name = await stepik_client.get_username(user_id=int(text))
-    logger.debug(f'Stepik username: {stepik_user_name or "Not found user"}')
+    mentor_id = int(text)
+
+    stepik_username = await stepik_client.get_username(user_id=mentor_id)
+    logger.debug(f'Stepik username: {stepik_username or "Not found user"}')
+    if stepik_username:
+        dialog_manager.dialog_data['mentor_id'] = mentor_id
+        dialog_manager.dialog_data['stepik_username'] = stepik_username
+        await dialog_manager.next()
 
     logger.debug('Exit')
 
 
 async def error_link_to_mentor(
     msg: Message,
-    widget: ManagedTextInput,
-    dialog_manager: DialogManager,
-    error: ValueError,
+    _widget: ManagedTextInput,
+    _dialog_manager: DialogManager,
+    _error: ValueError,
 ) -> None:
     logger.debug('Entry')
 
     await msg.delete()
 
     # TODO: write down the msg_id and delete it on the next update
-    await msg.answer(f'Вы ввели некорректную ссылку:\n'
-                     f'<i>{msg.text}</i>\n')
+    await msg.answer(f'Вы ввели некорректную ссылку:\n<i>{msg.text}</i>\n')
 
     logger.debug('Exit')

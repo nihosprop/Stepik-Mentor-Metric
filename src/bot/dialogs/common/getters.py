01d642a675fa5bbcfb7bc8f9c12b3792ae207d37
nihosprop <niwibonihs@tutamail.com>
2026-02-21 16:52:54 +0300

    refactor

--- src/bot/dialogs/common/getters.py
+++ src/bot/dialogs/common/getters.py
@@ -3,13 +3,16 @@ import logging
 from typing import Any
 
 from aiogram.types import User
+from aiogram_dialog import DialogManager
 from dishka.integrations.aiogram import inject
 
 logger = logging.getLogger(__name__)
 
 
 @inject
-async def get_tg_username(event_from_user: User, **_kwargs) -> dict[str, Any]:
+async def get_tg_username(
+    dialog_manager: DialogManager, event_from_user: User, **_kwargs
+) -> dict[str, Any]:
     username = 'Anonymous'
     if event_from_user.username and event_from_user.username.strip():
         username = f'@{event_from_user.username.strip()}'

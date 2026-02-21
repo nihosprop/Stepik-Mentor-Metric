900c5379524ea006506a1938e59a485102da3e71
nihosprop <niwibonihs@tutamail.com>
2026-02-21 19:40:59 +0300

    feat: add stepik course link validator
    Introduce a new utility function `check_stepik_course_link`.
    Validate and extract the course ID from various Stepik course URLs.
    Accepts links in formats such as `https://stepik.org/course/12345`, `https://stepik.org/course/12345/promo`, and `https://stepik.org/course/12345/syllabus`.
    Raise a `ValueError` if the link format is invalid or no course ID is found.

--- src/bot/dialogs/common/validators.py
+++ src/bot/dialogs/common/validators.py
@@ -13,3 +13,23 @@ def check_stepik_profile_link(link: str) -> str:
         return stepik_user_id
 
     raise ValueError
+
+def check_stepik_course_link(link: str) -> str:
+    """
+        Retrieves the course ID from the Stepik link.
+        Accepts links like:
+        - https://stepik.org/course/12345
+        - https://stepik.org/course/12345/promo
+        - https://stepik.org/course/12345/syllabus
+    """
+    match = re.search(
+        r'\bhttps?://[^\s/]+/course/(\d+)(?:/[^/\s]*)?/?\b',
+        link,
+        re.IGNORECASE,
+        )
+
+    if match:
+        stepik_course_id = match.group(1)
+        return stepik_course_id
+
+    raise ValueError

e008cd8bcaafbc21060970df0fca2467eebcb3cd
nihosprop <niwibonihs@tutamail.com>
2026-02-22 20:18:53 +0300

    refactor: adjust imports in mentors dialog

--- src/bot/dialogs/flows/mentors/dialog.py
+++ src/bot/dialogs/flows/mentors/dialog.py
@@ -9,10 +9,9 @@ from aiogram_dialog.widgets.kbd import (
     Select,
     SwitchTo,
 )
-from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.text import Const, Format, List
 
 from bot.dialogs.common.getters import get_tg_username
-from bot.dialogs.common.handlers import on_click_in_dev
 from bot.dialogs.common.validators import check_stepik_profile_link
 from bot.dialogs.common.widgets import BACK_BUTTON, MAIN_MENU_BUTTON
 from bot.dialogs.flows.mentors.getters import (

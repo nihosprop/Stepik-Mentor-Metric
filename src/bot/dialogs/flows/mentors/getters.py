cf5e5eef74e0f4846598fab449d5651d3c39faac
nihosprop <niwibonihs@tutamail.com>
2026-02-22 19:19:05 +0300

    feat: add mentors list getter
    Introduce a new asynchronous function to retrieve all registered mentors.

--- src/bot/dialogs/flows/mentors/getters.py
+++ src/bot/dialogs/flows/mentors/getters.py
@@ -5,6 +5,10 @@ from typing import Any
 from aiogram.types import User
 from aiogram_dialog import DialogManager
 from dishka.integrations.aiogram import inject
+from dishka.integrations.aiogram_dialog import FromDishka
+
+from db.models import StepikUser
+from db.repository.stepik_user_repo import StepikUserRepo
 
 logger = logging.getLogger(__name__)
 
@@ -16,3 +20,14 @@ async def get_stepik_username(
     return {
         'stepik_username': dialog_manager.dialog_data.get('stepik_username')
     }
+
+
+@inject
+async def get_mentors_list(
+    dialog_manager: DialogManager,
+    event_from_user: User,
+    stepik_user_repo: FromDishka[StepikUserRepo],
+    **_kwargs,
+) -> dict[str, list[StepikUser] | int]:
+    mentors = await stepik_user_repo.get_all_mentors()
+    return {'mentors': mentors, 'count': len(mentors)}

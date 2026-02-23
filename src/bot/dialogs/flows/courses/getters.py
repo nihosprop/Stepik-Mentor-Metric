4b927433fe628933287f681679750b162b2dbd75
nihosprop <niwibonihs@tutamail.com>
2026-02-23 16:29:59 +0300

    feat: add getter for all courses

--- src/bot/dialogs/flows/courses/getters.py
+++ src/bot/dialogs/flows/courses/getters.py
@@ -1,19 +1,31 @@
-from infrastructure.stepik.client import StepikAPIClient
-from dishka.integrations.aiogram_dialog import FromDishka
 import logging
 
+from collections.abc import Sequence
 from typing import Any
 
 from aiogram.types import User
 from aiogram_dialog import DialogManager
-from dishka.integrations.aiogram_dialog import inject
+from dishka.integrations.aiogram_dialog import FromDishka, inject
+
+from db.models import Course
+from db.repository.course_repo import CourseRepo
 
 logger = logging.getLogger(__name__)
+
 
 @inject
 async def get_course_title(
     dialog_manager: DialogManager, event_from_user: User, **_kwargs
 ) -> dict[str, Any]:
-    return {
-        'course_title': dialog_manager.dialog_data.get('course_title')
-    }
+    return {'course_title': dialog_manager.dialog_data.get('course_title')}
+
+
+@inject
+async def get_courses(
+    dialog_manager: DialogManager,
+    event_from_user: User,
+    course_repo: FromDishka[CourseRepo],
+    **_kwargs,
+) -> dict[str, Sequence[Course] | int]:
+    courses = await course_repo.get_all_courses()
+    return {'mentors': courses, 'count': len(courses)}

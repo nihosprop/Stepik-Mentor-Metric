a006817fd81b9e218dde072557a09d07b599eeb3
nihosprop <niwibonihs@tutamail.com>
2026-02-23 16:26:02 +0300

    feat: add new course management states
    Introduce states for selecting, confirming deletion, and listing courses.
    These additions expand the bot's capabilities for managing user courses within the dialog flow.

--- src/bot/dialogs/flows/courses/states.py
+++ src/bot/dialogs/flows/courses/states.py
@@ -5,3 +5,6 @@ class CoursesSG(StatesGroup):
     start = State()
     fill_link_to_course = State()
     confirm_curse = State()
+    selection_courses = State()
+    confirm_delete_course = State()
+    list_courses = State()

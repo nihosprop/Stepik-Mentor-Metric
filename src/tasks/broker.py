e682d92bff39bc5f1d5cdf52fff10c5428b0c34a
nihosprop <niwibonihs@tutamail.com>
2026-02-28 19:56:39 +0300

    chore: set up logging for taskiq broker

--- src/tasks/broker.py
+++ src/tasks/broker.py
@@ -2,8 +2,11 @@ from dishka import make_async_container
 from dishka.integrations.taskiq import setup_dishka
 from taskiq_redis import RedisStreamBroker
 
+from core.logger import setup_logging
 from core.main_config import main_config
 from infrastructure.di.providers import PROVIDERS
+
+setup_logging()
 
 broker = RedisStreamBroker(
     url=f'redis://{main_config.redis.host}:{main_config.redis.port}'

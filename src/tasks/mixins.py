a18b6cd3792b17df7da8dcfb4ddba3390ce88a1e
nihosprop <niwibonihs@tutamail.com>
2026-02-28 12:50:57 +0300

    refactor: remove explicit schedule_id generation

--- src/tasks/mixins.py
+++ src/tasks/mixins.py
@@ -6,7 +6,6 @@ from taskiq.scheduler.scheduled_task.v2 import ScheduledTask
 
 
 class MyScheduledTask(ScheduledTask):
-    schedule_id: str = uuid.uuid4().hex
     labels: dict[str, Any] = {}
     args: list[Any] = []
     kwargs: dict[str, Any] = {}

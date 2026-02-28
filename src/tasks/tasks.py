568cdd2993e60f7e18eb2b4558f9df61c9a074bb
nihosprop <niwibonihs@tutamail.com>
2026-02-28 13:12:08 +0300

    feat: make schedule setup idempotent
    Ensure default schedules are added only if they do not already exist.
    Prevent duplicate schedule entries on worker startup, improving robustness.
    Assign a unique schedule_id to each default task for proper identification.

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -31,13 +31,21 @@ async def test_ping_admin(
 @broker.on_event(TaskiqEvents.WORKER_STARTUP)
 async def setup_schedules(_state: TaskiqState) -> None:
     logger.info('Setting up schedules...')
-
     await scheduler_source.startup()
 
-    task = MyScheduledTask(
-        task_name=test_ping_admin.task_name,
-        cron='* * * * *',
-    )
-    await scheduler_source.add_schedule(task)
+    default_tasks = [
+        MyScheduledTask(
+            task_name=test_ping_admin.task_name,
+            schedule_id='1f779070-5683-4d6e-bc51-3e5e95175564',
+            cron='* * * * *',
+        )
+    ]
+    existing_schedules = await scheduler_source.get_schedules()
+    existing_ids = {s.schedule_id for s in existing_schedules}
 
-    logger.info('Schedule added for test_ping_admin')
+    for task in default_tasks:
+        if task.schedule_id not in existing_ids:
+            await scheduler_source.add_schedule(task)
+            logger.info(f'Schedule added for {task.task_name}')
+        else:
+            logger.info(f'Task {task.task_name} already in DB')

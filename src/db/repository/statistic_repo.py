8dd049bb37ee10e8a3281b7ad5dd257ee2db5a6a
nihosprop <niwibonihs@tutamail.com>
2026-03-03 19:38:35 +0300

    feat: add statistic repository methods
    Implement `StatisticRepo` class to manage database queries for mentor activity statistics.
    Provide `get_current_day_stats` to fetch mentor reply counts for the current day.
    Introduce `get_stats_for_period` to retrieve mentor reply statistics within a specified date range.
    These methods query mentor replies, group by mentor and course, and order by course title and reply count.

--- src/db/repository/statistic_repo.py
+++ src/db/repository/statistic_repo.py
@@ -1,2 +1,45 @@
+from collections.abc import Sequence
+from dataclasses import dataclass
+from datetime import UTC, datetime, time
+
+from sqlalchemy import and_, desc, func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import AuthorReply, Course, StepikUser
+
+
+@dataclass
 class StatisticRepo:
-    pass
+    session: AsyncSession
+
+    async def get_current_day_stats(self) -> Sequence:
+        """Gets statistics for the current day
+        (from 00:00 to the current moment)."""
+        now = datetime.now(UTC)
+        start_of_day = datetime.combine(now.date(), time.min, tzinfo=UTC)
+
+        return await self.get_stats_for_period(start_of_day, now)
+
+    async def get_stats_for_period(
+        self, start_date: datetime, end_date: datetime
+    ) -> Sequence:
+        stmt = (
+            select(
+                StepikUser.full_name,
+                Course.title.label('course_title'),
+                func.count(AuthorReply.comment_id).label('replies_count'),
+            )
+            .join(AuthorReply, StepikUser.user_id == AuthorReply.author_id)
+            .join(Course, Course.course_id == AuthorReply.course_id)
+            .where(
+                and_(
+                    AuthorReply.is_mentor_reply,
+                    AuthorReply.comment_created_at >= start_date,
+                    AuthorReply.comment_created_at <= end_date,
+                )
+            )
+            .group_by(Course.title, StepikUser.full_name)
+            .order_by(Course.title, desc('replies_count'))
+        )
+        result = await self.session.execute(stmt)
+        return result.all()

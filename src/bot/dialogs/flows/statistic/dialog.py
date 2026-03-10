7ffd5b565b014a7fd4bf5751e23b11dc91df5da7
nihosprop <niwibonihs@tutamail.com>
2026-03-10 19:25:03 +0300

    feat(dialogs): add monthly statistic reports
    Expand statistic dialog to include options for current and last month reports.
    Implement handlers to fetch and display monthly reports from StatisticService.
    Remove unused imports from the statistic dialog module.

--- src/bot/dialogs/flows/statistic/dialog.py
+++ src/bot/dialogs/flows/statistic/dialog.py
@@ -1,47 +1,50 @@
-from aiogram import F
 from aiogram_dialog import Dialog, Window
-from aiogram_dialog.widgets.input import TextInput
 from aiogram_dialog.widgets.kbd import (
-    Back,
-    Group,
-    NextPage,
-    PrevPage,
-    Row,
-    ScrollingGroup,
-    Select,
+    Button,
     SwitchTo,
 )
-from aiogram_dialog.widgets.text import Const, Format, List
+from aiogram_dialog.widgets.text import Const
 
-from bot.dialogs.common.getters import get_tg_username
-from bot.dialogs.common.validators import check_stepik_profile_link
 from bot.dialogs.common.widgets import BACK_BUTTON, MAIN_MENU_BUTTON
-from bot.dialogs.flows.mentors.getters import (
-    get_list_mentors,
-    get_mentors,
-    get_stepik_username,
-)
-from bot.dialogs.flows.mentors.handlers import (
-    add_mentor_to_db,
-    correct_link_to_mentor,
-    error_link_to_mentor,
-    on_delete_mentor,
-    on_mentor_selected,
+from bot.dialogs.flows.statistic.handlers import (
+    send_current_month,
+    send_last_month,
 )
 from bot.dialogs.flows.statistic.states import StatisticSG
 
 statistic_dialog = Dialog(
     Window(
-        Format(text='===  Меню Статистики  ==='),
+        Const(text='===  Меню Статистики  ==='),
         SwitchTo(
-            text=Const('Общая'),
-            id='general_stat',
-            state=StatisticSG.general
+            text=Const('Общая'), id='general_stat', state=StatisticSG.general
         ),
         SwitchTo(
             text=Const('Индивидуальная'),
             id='individual',
-            state=StatisticSG.individual),
+            state=StatisticSG.individual,
+        ),
         MAIN_MENU_BUTTON,
-        state=StatisticSG.start)
-    )
+        state=StatisticSG.start,
+    ),
+    Window(
+        Const('Выберите отчет'),
+        Button(
+            text=Const('За текущий месяц'),
+            id='curr_month',
+            on_click=send_current_month,  # type: ignore[arg-type]
+        ),
+        Button(
+            text=Const('За прошедший месяц'),
+            id='last_month',
+            on_click=send_last_month,  # type: ignore[arg-type]
+        ),
+        MAIN_MENU_BUTTON,
+        BACK_BUTTON,
+        state=StatisticSG.general,
+    ),
+    Window(
+        MAIN_MENU_BUTTON,
+        BACK_BUTTON,
+        state=StatisticSG.individual,
+    ),
+)
--- src/bot/dialogs/flows/statistic/handlers.py
+++ src/bot/dialogs/flows/statistic/handlers.py
@@ -3,7 +3,38 @@ import logging
 from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager
 from aiogram_dialog.widgets.kbd import Button
+from dishka.integrations.aiogram_dialog import FromDishka, inject
 
-from bot.dialogs.flows.statistic.states import StatisticSG
+from services.statistic_service import StatisticService
 
 logger = logging.getLogger(__name__)
+
+
+@inject
+async def send_current_month(
+    clbk: CallbackQuery,
+    _button: Button,
+    dialog_manager: DialogManager,
+    statistic_service: FromDishka[StatisticService],
+) -> None:
+    logger.debug('Entry')
+
+    report = await statistic_service.get_monthly_report_text()
+    await clbk.answer(report)
+
+    logger.debug('Exit')
+
+
+@inject
+async def send_last_month(
+    clbk: CallbackQuery,
+    _button: Button,
+    dialog_manager: DialogManager,
+    statistic_service: FromDishka[StatisticService],
+) -> None:
+    logger.debug('Entry')
+
+    report = await statistic_service.get_monthly_report_text(prev_month=False)
+    await clbk.answer(report)
+
+    logger.debug('Exit')

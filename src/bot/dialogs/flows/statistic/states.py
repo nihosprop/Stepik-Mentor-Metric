bc6cacaa17558075af2979a537e9d92485fde943
nihosprop <niwibonihs@tutamail.com>
2026-03-10 17:43:06 +0300

    feat(states): add states for current and last month

--- src/bot/dialogs/flows/statistic/states.py
+++ src/bot/dialogs/flows/statistic/states.py
@@ -4,4 +4,6 @@ from aiogram.fsm.state import State, StatesGroup
 class StatisticSG(StatesGroup):
     start = State()
     general = State()
+    current_month = State()
+    last_month = State()
     individual = State()

78090968c5337594bfcd8a1e2fba78b7dda21cb6
nihosprop <niwibonihs@tutamail.com>
2026-03-14 17:27:54 +0300

    refactor

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -97,9 +97,7 @@ async def poll_stepik_courses(
 
                 if comment_time > last_time:
                     logger.debug(
-                        f'NEW_COMMENT:'
-                        f'{json.dumps(comment, indent=2, ensure_ascii=False)}'
-                    )
+                        f'NEW_COMMENT: {comment["id"]=}, {comment["parent"]=}')
 
                     author_id = comment['user']
                     author_username = await stepik_client.get_username(

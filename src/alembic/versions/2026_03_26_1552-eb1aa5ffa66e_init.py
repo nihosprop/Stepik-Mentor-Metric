8e90d85932234d6545811da3b414ad881f59f4e9
nihosprop <niwibonihs@tutamail.com>
2026-03-26 22:53:50 +0300

    chore: update project version and alembic init migration
    Bump project version from `v0.2.1b` to `v0.2.4b`.
    Replace the initial Alembic migration file.
    The new migration `eb1aa5ffa66e` is functionally identical to the removed `ff6299978d7f`.
    This ensures a consistent base for future schema changes.

--- src/alembic/versions/2026_03_26_1552-eb1aa5ffa66e_init.py
+++ src/alembic/versions/2026_03_26_1552-eb1aa5ffa66e_init.py
@@ -1,8 +1,8 @@
 """init
 
-Revision ID: ff6299978d7f
+Revision ID: eb1aa5ffa66e
 Revises: 
-Create Date: 2026-03-15 11:55:52.542617
+Create Date: 2026-03-26 15:52:08.756499
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ff6299978d7f'
+revision: str = 'eb1aa5ffa66e'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None

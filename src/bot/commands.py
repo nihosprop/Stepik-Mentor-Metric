965784c296ece9011271a356d236638a57a64b8e
nihosprop <niwibonihs@tutamail.com>
2026-03-26 21:39:11 +0300

    refactor

--- src/bot/commands.py
+++ src/bot/commands.py
@@ -4,6 +4,6 @@ from aiogram.types import BotCommand
 
 async def set_main_menu(bot: Bot) -> None:
     main_menu_commands = [
-        BotCommand(command='/start', description='Запустить бота'),
+        BotCommand(command='/start', description='Главное меню'),
     ]
     await bot.set_my_commands(main_menu_commands)

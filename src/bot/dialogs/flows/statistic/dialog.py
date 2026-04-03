653e7c1c007759d8eb751aa7a55338a301b6799f
nihosprop <niwibonihs@tutamail.com>
2026-04-03 14:37:26 +0300

    chore: mark detailed statistic buttons as under development
    Replace specific on_click handlers for current and last month detailed statistics.
    This change temporarily redirects these actions to a placeholder function (on_click_in_dev).
    It indicates that the detailed statistics functionality is currently under active development or refinement.

--- src/bot/dialogs/flows/statistic/dialog.py
+++ src/bot/dialogs/flows/statistic/dialog.py
@@ -44,12 +44,12 @@ statistic_dialog = Dialog(
         Button(
             text=Const('Подробная (Текущий месяц)'),
             id='curr_month',
-            on_click=send_current_month_detailed_stats,  # ty:ignore[invalid-argument-type]
+            on_click=on_click_in_dev,  # ty:ignore[invalid-argument-type]
         ),
         Button(
             text=Const('Подробная (Прошедший месяц)'),
             id='last_month',
-            on_click=send_last_month_detailed_stats,  # ty:ignore[invalid-argument-type]
+            on_click=on_click_in_dev,  # ty:ignore[invalid-argument-type]
         ),
         MAIN_MENU_BUTTON,
         BACK_BUTTON,

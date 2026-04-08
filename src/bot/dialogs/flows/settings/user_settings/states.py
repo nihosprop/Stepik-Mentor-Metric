c2f6351d1cb940f96417e431eb0c5ba65ec4308e
nihosprop <niwibonihs@tutamail.com>
2026-04-08 14:52:47 +0300

    feat(dialogs): add new states for user settings flow
    Introduce 'confirm_add_user' state to handle confirmation of new mentor additions.
    Introduce 'list_mentors' state to display the current list of mentors associated with the user.

--- src/bot/dialogs/flows/settings/user_settings/states.py
+++ src/bot/dialogs/flows/settings/user_settings/states.py
@@ -4,4 +4,6 @@ from aiogram.fsm.state import State, StatesGroup
 class UserSettingsSG(StatesGroup):
     start = State()
     add_user = State()
+    confirm_add_user = State()
     remove_user = State()
+    list_mentors = State()

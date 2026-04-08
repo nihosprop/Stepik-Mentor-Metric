3cdb2f0e8d02ed08af29d8bb729000f048199ee2
nihosprop <niwibonihs@tutamail.com>
2026-04-08 11:57:01 +0300

    feat(dialogs): conditionally display settings button for admins
    Add a visibility condition to the 'Настройки' button.
    Ensure the settings button is only shown to users with administrator privileges.
    Replace the `get_tg_username` getter with `get_access_flags` to provide necessary access flags.

--- src/bot/dialogs/flows/start/dialog.py
+++ src/bot/dialogs/flows/start/dialog.py
@@ -2,10 +2,10 @@ from aiogram_dialog import Dialog, Window
 from aiogram_dialog.widgets.kbd import Group, Row, Start
 from aiogram_dialog.widgets.text import Const
 
-from bot.dialogs.common.getters import get_tg_username
 from bot.dialogs.flows.courses.states import CoursesSG
 from bot.dialogs.flows.mentors.states import MentorSG
 from bot.dialogs.flows.settings.states import SettingsSG
+from bot.dialogs.flows.start.getters import get_access_flags
 from bot.dialogs.flows.start.states import StartSG
 from bot.dialogs.flows.statistic.states import StatisticSG
 
@@ -34,10 +34,11 @@ start_dialog = Dialog(
                 text=Const('Настройки'),
                 id='settings',
                 state=SettingsSG.start,
+                when='is_admin',
             ),
         ),
-        getter=get_tg_username,
+        getter=get_access_flags,
         state=StartSG.start,
     ),
-    name='start'
+    name='start',
 )
